d88458ee7081307e684ff1f9a2e5efc3f22ddd0e
topcue <topcue1019@naver.com>
2022-09-06 12:29:34 +0900

    fix env

--- simple_test/run_simple_test.sh
+++ simple_test/run_simple_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Set base workspace
+export BASE=$HOME/metamorphic-fuzz-testing
+
 ## Build PUT w/ libfuzzer
 $BASE/clang-llvm/bin/clang -g -fsanitize-coverage=trace-pc-guard test_fuzzer.cpp -c
 
@@ -14,3 +17,4 @@ rm -rf in out; mkdir in out; echo z > in/z;
 $BASE/afl/afl-fuzz -i in -o out ./test_fuzzer
 
 # EOF
+
